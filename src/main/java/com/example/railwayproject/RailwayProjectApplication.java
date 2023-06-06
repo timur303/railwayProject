8be84a8208f784c010852ae3daa83d14f7e72288
Тимур <kadyrbekovtimur001@gmail.com>
2023-06-07 04:24:16 +0600

    Initial commit

---
+++ src/main/java/com/example/railwayproject/RailwayProjectApplication.java
@@ -0,0 +1,19 @@
+package com.example.railwayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class RailwayProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayProjectApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String get() {
+        return "Hello";
+    }
+
+}
---
+++ src/test/java/com/example/railwayproject/RailwayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railwayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
