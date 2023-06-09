f24310a4992ae6b5ab9d329c00183d30d050f553
Timur <63163159+timur303@users.noreply.github.com>
2023-06-09 07:40:05 +0600

    Update RailwayProjectApplication.java

--- src/main/java/com/example/railwayproject/RailwayProjectApplication.java
+++ src/main/java/com/example/railwayproject/RailwayProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Restcontroller
 public class RailwayProjectApplication {
 
     public static void main(String[] args) {
